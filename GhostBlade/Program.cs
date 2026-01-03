bdb6436f33e9df8846695a6dfa70e41b0812e65c
chile <chile@GHOSTCOW>
2026-01-03 03:19:08 -0500

    Add project files.

---
+++ GhostBlade/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace GhostBlade
+{
+   internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello from C#");
+
+            Process p = new Process();
+            p.StartInfo.FileName = @"C:\Development2\GhostX\X64\Release\GhostBladeX.exe";
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            Console.WriteLine("Output from C++:");
+            Console.WriteLine(output);
+        }
+    }
+}
+
+
